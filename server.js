fe5473c9bedf0107aba5e927de6326c4c5d11d96
unknown <raj.ankur8@gmail.com>
2018-08-01 16:24:00 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require ('express');
+const hbs = require ('hbs');
+var app = express();
+const fs = require ('fs');
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getfullyr', () =>{
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase()
+})
+
+app.use((req,res,next) =>{
+var now = new Date().toString();
+
+var log =`${now}:${req.method},${req.url}`
+console.log(log);
+fs.appendFileSync('server.log',log +'\n');
+next();
+});
+
+// app.use((req,res,next) =>{
+// // var
+// res.render('maintenance.hbs',{
+//  pageTitle : "Maintenance Page",
+   
+//     msg : "Hello you are at the Maintenance page. Please wait for some time"    
+// })
+// });
+
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res) =>{
+    // res.send('<h1>Hello express</h1>');
+    res.render('home.hbs',{
+    pageTitle : "Home Page",
+   
+    msg : "Hello you are at the home page."
+}
+     
+    )
+});
+
+app.get('/bad',(req,res) =>{
+
+res.send({
+    errorMessage : "unable to fetch data",
+})
+});
+app.get('/about',(req,res) =>{
+
+res.render('about.hbs',{
+    pageTitle : "About Page",
+    
+});
+})
+app.listen(3000,() =>{
+    console.log('server is up');
+});
